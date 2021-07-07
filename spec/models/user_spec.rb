eef02d9ec68d6cbb6afabb1e59e8c5c33afed71a
Titeinohito <84887486+Titeinohito@users.noreply.github.com>
2021-07-07 11:40:33 +0900

    ログイン修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -114,6 +114,12 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
     
+    it "重複したメールアドレスがあるときに登録できない" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
   end
 
 end
