ef312025f18ecb217597ae7570c1c0dbc81e450b
Titeinohito <84887486+Titeinohito@users.noreply.github.com>
2021-07-27 16:19:35 +0900

    コントローラ修正、パス修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :new, :create, :item_params]
 
   def index
     @items = Item.all.order(created_at: 'DESC')
@@ -19,16 +20,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to action: :index if current_user.id != @item.user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to action: :index
     else
@@ -41,5 +40,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id,
                                  :charge_id, :area_id, :ship_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
