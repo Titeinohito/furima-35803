e3a0ffd16ed5fa8c8797d67c430a8a252939b409
Titeinohito <84887486+Titeinohito@users.noreply.github.com>
2021-07-27 17:35:12 +0900

    Revert "Revert "削除機能実装""
    This reverts commit 964dfcdb0bcfe212ca10d0a7e5c588cb3038262d.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
-  before_action :redirecting, only: [:edit, :update]
+  before_action :redirecting, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: 'DESC')
@@ -28,6 +28,14 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
       redirect_to action: :index
     else
       render action: :edit
